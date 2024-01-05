7790b4f77a2f65aeb61ba8e6a30fa8e638a0e747
Zack Moyal <zack.moyal@gmail.com>
2024-01-05 06:59:59 -0800

    initial commit

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace ArraysAndLists
 {
@@ -9,17 +11,18 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int array and populate it with numbers 1-10
-            
+            int[] myIntArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
             /* Create a list of type int
              * Name the list "evens"
              */
+            var evens = new List<int>();
 
             
             /* Create another list of type int
              * Name the list "odds"
              */
-            
+            var odds = new List<int>();
 
              /* Using either a foreach or for loop,
              * iterate through the array you populated with 10 numbers.
@@ -28,17 +31,36 @@ namespace ArraysAndLists
              * If the number is even, add it to the evens list.
              * If the number is odd, add it to the odds list.
              */
-
-
+             foreach (int number in myIntArray)
+            {
+                if (number % 2 == 0)
+                {
+                    evens.Add(number);
+                }
+                else
+                {
+                    odds.Add(number);
+                }
+            }
 
             /* Using a foreach loop,
              * display the numbers in your "evens" list
              */
-            
+            Console.WriteLine("Even Numbers:");
+            foreach (int evenNumber in evens)
+            {
+                Console.Write(evenNumber + " ");
+            }
+            Console.WriteLine();
 
             /* Using a for loop,
              * display the numbers in your "odds" list
              */
+            Console.WriteLine("Odd Numbers:");
+            for (int i = 0; i < odds.Count; i++)
+            {
+                Console.Write(odds[i] + " ");
+            }
         }
     }
 }
